ff70102e5dd2780ff6a2b6d1ab8dd8566dff21b1
muditkumar <mk2@breakingwavesoftware.com>
2014-09-01 13:06:07 +0530

    committing

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,13 @@
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find();
+  	var keyword  = Session.get("search-query");
+  //	console.log(keyword);
+    if (!keyword){
+     return Posts.find({},{sort: {tweet_id_str: -1}});
+    }
+    else{
+    	return Posts.find({text : {$regex : keyword , $options: 'i' }}).fetch();
+         //return Posts.find({'text': keyword});
+    }
   }
 });
